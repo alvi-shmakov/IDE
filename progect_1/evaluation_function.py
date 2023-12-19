19c7ce9e2e91dd765887fff34bad51d3e304d9b1
alvi-shmakov <a.shmakov@mail.ru>
2023-12-19 22:25:05 +0700

    create game_v3.ipynb

--- progect_1/evaluation_function.py
+++ progect_1/evaluation_function.py
@@ -19,4 +19,5 @@ def score_game(random_predict) -> int:
         count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток")
+    return score
